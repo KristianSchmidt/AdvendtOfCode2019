13d5fa0e17599835269bc08a4d431df1dbcd8315
Kristian Schmidt <kst@siteimprove.com>
2019-12-02 08:37:11 +0100

    Day two

--- Day2.fsx
+++ Day2.fsx
@@ -5,14 +5,71 @@ open System.IO
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = File.ReadAllLines("./input/2.txt")
+let data =
+    File.ReadAllText("./input/20.txt").Split(',')
+    |> Array.map int
+    |> Array.mapi (fun i x -> (i,x))
+    |> Map.ofArray
 
-let ans1 = data
+let transform (mInit : Map<int,int>) =
+    let rec f m pos =
+        match Map.find pos m with
+        | 99 -> m
+        | 1 -> // Addition
+            let op1 =
+                let op1Idx = Map.find (pos + 1) m
+                Map.find op1Idx m
+            let op2 =
+                let op2Idx = Map.find (pos + 2) m
+                Map.find op2Idx m
+            let storeIdx = Map.find (pos + 3) m
+            let res = op1 + op2
+            let newMap = Map.add storeIdx res m
+            //printfn "ADD: [%i]%i * [%i]%i = [%i]%i" op1Idx op1 op2Idx op2 store res
+
+            f newMap (pos + 4)
+        | 2 -> // Multiplication
+            let op1 =
+                let op1Idx = Map.find (pos + 1) m
+                Map.find op1Idx m
+            let op2 =
+                let op2Idx = Map.find (pos + 2) m
+                Map.find op2Idx m
+            let storeIdx = Map.find (pos + 3) m
+            let res = op1 * op2
+            let newMap = Map.add storeIdx res m
+            //printfn "MUL: [%i]%i * [%i]%i = [%i]%i" op1Idx op1 op2Idx op2 store res
+
+            f newMap (pos + 4)
+
+    f mInit 0
+
+let ans1 =
+    data
+    |> Map.add 1 12
+    |> Map.add 2 2
+    |> transform
+    |> Map.find 0
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let f noun verb =
+    data
+    |> Map.add 1 noun
+    |> Map.add 2 verb
+    |> transform
+    |> Map.find 0
+
+let nvInputs =
+    [ for n in 1 .. 100 do
+        for v in 1 .. 100 do 
+            yield (n,v)      ]
+
+let ans2 =
+    nvInputs
+    |> Seq.find (fun (n,v) -> f n v = 19690720)
+    |> (fun (n,v) -> 100 * n + v)
 
 ans2
