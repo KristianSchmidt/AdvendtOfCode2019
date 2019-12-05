747db1b4d1d29eb56fcbca29fd7b46f13c231153
Kristian Schmidt <kristian@kreutz.us>
2019-12-05 20:43:24 +0100

    Day 4 and Day 5

--- Day4.fsx
+++ Day4.fsx
@@ -5,14 +5,52 @@ open System.IO
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = File.ReadAllLines("./input/4.txt")
+//let data = File.ReadAllLines("./input/4.txt")
 
-let ans1 = data
+let r1 = 193651
+let r2 = 649729
+
+let nums =
+    [ for i in r1 .. r2 do yield i ]
+    |> List.map (fun i -> i.ToString().ToCharArray() |> Array.map (string >> int) |> Array.windowed 2)
+
+let isNotDescending (arr : int array array) =
+    arr
+    |> Array.exists (fun [|i1;i2|] -> i2 < i1)
+    |> not
+
+let hasTwoSame (arr : int array array) =
+    arr
+    |> Array.exists (fun [|i1;i2|] -> i1 = i2)
+
+let fulfillCriteria =
+    nums
+    |> List.where isNotDescending
+    |> List.where hasTwoSame
+
+let ans1 =
+    fulfillCriteria
+    |> List.length
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let spliceBack (arr : int array array) =
+    let re = arr |> Array.rev |> Array.head
+    let t = arr |> Array.rev |> Array.tail |> Array.rev |> Array.map Array.head
+    Array.concat [t; re]
+
+let hasAPair (arr : int array) =
+    arr
+    |> Array.groupBy id
+    |> Array.tryFind (fun (k,v) -> Array.length v = 2)
+    |> Option.isSome
+
+let ans2 =
+    fulfillCriteria
+    |> List.map spliceBack
+    |> List.where hasAPair
+    |> List.length
 
 ans2
--- Day5.fsx
+++ Day5.fsx
@@ -5,14 +5,96 @@ open System.IO
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = File.ReadAllLines("./input/5.txt")
+let data =
+    File.ReadAllText("./input/5.txt").Split(',')
+    |> Array.map int
+    |> Array.mapi (fun i x -> (i,x))
+    |> Map.ofArray
 
-let ans1 = data
+let separate (i : int) =
+    let ones = i % 10
+    let tens = (i/10)%10
+    let hundreds = (i/100)%10
+    let thousands = (i/1000)%10
+    let tenthousands = (i/10000)
+    (tenthousands, thousands, hundreds, 10*tens + ones)
+
+let findOp (pos : int) m mode =
+    let opIdx = Map.find pos m
+    match mode with
+    | 0 -> Map.find opIdx m
+    | 1 -> opIdx
+
+let transform input (mInit : Map<int,int>) =
+    let rec f m pos =
+        match separate <| Map.find pos m with
+        | (_, _, _, 99) -> m // Halt
+        | (outMode, sndMode, fstMode, instr) when instr = 1 || instr = 2 -> // ADD or MUL
+            let op = if (instr = 1) then (+) else (*)
+            let opTxt = if (instr = 1) then "ADD" else "MUL"
+
+            let op1 = findOp (pos + 1) m fstMode
+            let op2 = findOp (pos + 2) m sndMode
+            let storeIdx = Map.find (pos + 3) m
+            let res = op op1 op2
+            let newMap = Map.add storeIdx res m
+            printfn "%s: %i %i = [%i]%i" opTxt op1 op2 storeIdx res
+
+            f newMap (pos + 4)
+        | (outMode, sndMode, fstMode, 3) -> // Input
+            let storeIdx = Map.find (pos + 1) m
+            let newMap = Map.add storeIdx input m
+            printfn "INPUT: %i" input
+            f newMap (pos + 2)
+        | (outMode, sndMode, fstMode, 4) -> // Output
+            let outputIdx = Map.find (pos + 1) m
+            printfn "OUT: %i" (Map.find outputIdx m)
+            f m (pos + 2)
+        | (outMode, sndMode, fstMode, 5) -> // Jump if true
+            let op1 = findOp (pos + 1) m fstMode
+            let op2 = findOp (pos + 2) m sndMode
+            if (op1 <> 0) then
+                printfn "JIT: %i" op2
+                f m op2
+            else
+                f m (pos + 3)
+        | (outMode, sndMode, fstMode, 6) -> // Jump if false
+            let op1 = findOp (pos + 1) m fstMode
+            let op2 = findOp (pos + 2) m sndMode
+            if (op1 = 0) then
+                printfn "JIF: %i" op2
+                f m op2
+            else
+                f m (pos + 3)
+        | (outMode, sndMode, fstMode, 7) -> // Less than
+            let op1 = findOp (pos + 1) m fstMode
+            let op2 = findOp (pos + 2) m sndMode
+            let op3 = findOp (pos + 3) m 1
+            let newMap =
+                if (op1 < op2) then
+                    Map.add op3 1 m
+                else
+                    Map.add op3 0 m
+            f newMap (pos + 4)
+        | (outMode, sndMode, fstMode, 8) -> // Equal
+            let op1 = findOp (pos + 1) m fstMode
+            let op2 = findOp (pos + 2) m sndMode
+            let op3 = findOp (pos + 3) m 1
+            let newMap =
+                if (op1 = op2) then
+                    Map.add op3 1 m
+                else
+                    Map.add op3 0 m
+            f newMap (pos + 4)
+
+    f mInit 0
+
+let ans1 = data |> transform 1
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let ans2 = data |> transform 5
 
 ans2
