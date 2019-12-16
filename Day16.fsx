13ace53a4ca76d65c74ab1be9d99752f8ed27444
Kristian Schmidt <kst@siteimprove.com>
2019-12-16 23:27:48 +0100

    Day 16 done

--- Day16.fsx
+++ Day16.fsx
@@ -1,18 +1,72 @@
 #load "Helpers.fs"
 
+#time "on"
+
 open System
 open System.IO
+open System.Linq
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = File.ReadAllLines("./input/16.txt")
+let data =
+    File.ReadAllText("./input/16.txt")
+    |> (fun s -> s.Trim().ToCharArray() |> Array.map (string >> int))
 
-let ans1 = data
+/// Use i as 1 indexed
+let pattern n i =
+    let i' = (i % (4*n))
+    if (i' < n) then
+        0
+    else if (i' < 2*n) then
+        1
+    else if (i' < 3*n) then
+        0
+    else
+        -1
+
+let applyFFT (input : int array) = 
+    let applyForN n =
+        [| 1 .. input.Length |]
+        |> Array.sumBy (fun i -> (pattern n i)*input.[i-1])
+        |> (fun i -> (abs i) % 10)
+
+    [| 1 .. input.Length |]
+    |> Array.map applyForN
+
+let ans1 =
+    [| 1 .. 100 |]
+    |> Array.fold (fun s _ -> applyFFT s) data
+    |> Array.take 8
+    |> Array.map string
+    |> String.concat ""
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let data2 =
+    Array.replicate 10000 data
+    |> Array.concat
 
-ans2
+let readFromIdx =
+    data.[0 .. 6]
+    |> Array.map string
+    |> String.concat ""
+    |> int
+
+let accMod10 i acc = (acc + i) % 10 
+
+// Takes list in reverse as input  
+let partialFFT (arr : int list) =
+    List.scan accMod10 arr.Head arr.Tail
+
+let replicated =
+    List.ofArray data2.[readFromIdx .. ]
+    |> List.rev
+
+[ 1 .. 100 ]
+|> List.fold (fun s _ -> partialFFT s) replicated
+|> List.rev // Need to re-reverse the list
+|> List.take 8
+|> List.map string
+|> String.concat ""
